ac9ca3e50089f031bc1fd500d85bceb7b1387a22
Arindam Halder <halderarindam10000@gmail.com>
2022-09-13 17:18:55 +0530

    search-bar added

--- src/components/Problems/Problem.jsx
+++ src/components/Problems/Problem.jsx
@@ -1,5 +1,6 @@
 import ProblemDesc from "./ProblemDesc";
 import "./Problem.css";
+import { useState } from "react";
 
 export default function Problem(props) {
 
@@ -7,14 +8,34 @@ export default function Problem(props) {
 
     // onDelProblem: it will call and tell its parent(App.js) that my child told me something is deleted there 
 
+    const [searchedData, setSearchedData] = useState("");
+
+    function handleChange(e){
+        setSearchedData(e.target.value);
+    }
+
     return (
         <div>
             <div className="problems-page">
                 <div className="problems-container">
                     <h4 className="problem-head">Your Problems</h4>
 
+                    <h4 className="search-bar">
+                        <div className="search-box">
+                            <input type="text" placeholder="What are you looking for?" value={searchedData} onChange={handleChange}/>
+                            <button><img src="search.png" alt="img" /></button>
+                        </div>
+                    </h4>
+
                     {
-                        problemArray && (problemArray.length === 0 ? <p className="no-problem-text">No Problem Found</p> : problemArray.map((problem) => {
+                        problemArray && (problemArray.length === 0 ? <p className="no-problem-text">No Problem Found</p> : problemArray.filter((problem)=>{
+                            if(searchedData.trim()===""){
+                                return problem;
+                            }
+                            else if(problem.name.toLowerCase().includes(searchedData.trim().toLowerCase())){
+                                return problem;
+                            }
+                        }).map((problem) => {
                             return (
                                 <ProblemDesc key={problem._id} name={problem.name} link={problem.link} id={problem._id} onDelProblem={props.onDeleteProblem}/>
                             )
